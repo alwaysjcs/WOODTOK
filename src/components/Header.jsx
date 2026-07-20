168ffd198b683cfa9593999241fd27877b7787eb
alwaysjcs <alwaysjcs@gmail.com>
2026-07-20 20:21:42 +0900

    Sprint-9 step-006

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,49 @@
-import { useRef } from "react";
 import logo from "../assets/woodtok-logo.png";
 
 function Header({ selectedDate, setSelectedDate }) {
-  const dateRef = useRef(null);
 
   const week = ["일", "월", "화", "수", "목", "금", "토"];
 
   const date = new Date(selectedDate);
 
   const displayDate =
-     selectedDate.replaceAll("-", ".") +
-     ` (${week[date.getDay()]})`;
+    selectedDate.replaceAll("-", ".") +
+    ` (${week[date.getDay()]})`;
 
   return (
-  <header className="header">
-    <div className="header-left">
-      <img
-        src={logo}
-        alt="WOODTOK"
-        className="header-logo"
-      />
-    </div>
+    <header className="header">
 
-    <div className="header-date"
-    >
-      📅 {displayDate}
+      <div className="header-left">
+        <img
+          src={logo}
+          alt="WOODTOK"
+          className="header-logo"
+        />
+      </div>
 
-      <input
-          ref={dateRef}
+      <label
+        className="header-date"
+        htmlFor="header-date-input"
+      >
+        📅 {displayDate}
+
+        <input
+          id="header-date-input"
           type="date"
           value={selectedDate}
           onChange={(e) => {
-      
-              if (!e.target.value) {
-                  return;
-              }
-      
-              setSelectedDate(e.target.value);
-      
+
+            if (!e.target.value) return;
+
+            setSelectedDate(e.target.value);
+
           }}
           className="header-date-input"
-      />
-    </div>
-  </header>
-);
+        />
+      </label>
+
+    </header>
+  );
 }
 
 export default Header;
